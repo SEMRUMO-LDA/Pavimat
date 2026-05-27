3ae9abf8949be650263e73d0284f0efa4d896148
tiagojsp <tiagojspacheco@gmail.com>
2026-05-27 15:01:26 +0100

    build: trigger Vercel redeploy with env vars

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
+// Trigger Vercel redeploy to compile new KibanCMS environment variables
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
